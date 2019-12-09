c1d080097cd30ec0db1467aa786d782ed7862769
Jason Pierce <jpierce@vibrenthealth.com>
2019-12-09 16:42:22 -0500

    getting map to render
    trying to add state loader stuff

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import constants from "./constants";
 
 import "./styles.css";
 
 function App() {
-  return (
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: constants.GMAPS_KEY // ,
+    // ...otherOptions
+  });
+
+  // const onLoad = React.useCallback(function onLoad(mapInstance) {
+  //   // do something with map Instance
+  // });
+
+  if (loadError) {
+    return <div> Map can't be rendered </div>;
+  }
+
+  return isLoaded ? (
     <div className="mapContainer">
-      <LoadScript id="script-loader" googleMapsApiKey={constants.GMAPS_KEY}>
-        <GoogleMap
-          id="example-map"
-          center={constants.defaultCenter}
-          zoom={constants.defaultZoomLevel}
-        />
-      </LoadScript>
+      <GoogleMap
+        id="example-map"
+        center={constants.defaultCenter}
+        zoom={constants.defaultZoomLevel}
+        // onLoad={onLoad}
+      />
     </div>
+  ) : (
+    <div> Something went wrong </div>
   );
 }
 
